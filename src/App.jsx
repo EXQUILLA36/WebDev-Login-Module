aa9a9bc5223191fbd41078c844271c96504985ff
EXQUILLA36 <Trizxd123@gmail.com>
2025-08-02 21:15:41 +0800

    Your commit message here

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ function App() {
       <div className='w-[85%] h-[80%]'>
         <form action="submit" className="flex flex-col justify-around items-center h-full">
           <input type="text" name="name" id="name" placeholder='Full Name' />
-          <input type="text" name="mail" id="mail" placeholder='Email Address' />
+          <input type="password" name="passw" id="mail" placeholder='Password' />
           <div className='flex felx-row justify-between w-full gap-2'>
             <div className="w-[50%]"><input type="checkbox" name='tnc' id='tnc' className='w-[12%] ' /> <label htmlFor="tnc">Remember Me</label></div>
             <a href="" className=" text-white ">Forgot Password?</a>
